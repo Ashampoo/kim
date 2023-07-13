1fcd1bd78cffbc0eb44a2c2347c28d41f17c79af
Stefan Oltmann <github@stefan-oltmann.de>
2023-07-13 11:52:51 +0200

    Commented currently unused method

--- src/commonMain/kotlin/com/ashampoo/kim/output/LittleEndianBinaryByteWriter.kt
+++ src/commonMain/kotlin/com/ashampoo/kim/output/LittleEndianBinaryByteWriter.kt
@@ -24,11 +24,11 @@ class LittleEndianBinaryByteWriter(byteWriter: ByteWriter) : BinaryByteWriter(by
         write(0xFF and (value shr 8))
     }
 
-    override fun write3Bytes(value: Int) {
-        write(0xFF and value)
-        write(0xFF and (value shr 8))
-        write(0xFF and (value shr 16))
-    }
+//    override fun write3Bytes(value: Int) {
+//        write(0xFF and value)
+//        write(0xFF and (value shr 8))
+//        write(0xFF and (value shr 16))
+//    }
 
     override fun write4Bytes(value: Int) {
         write(0xFF and value)
