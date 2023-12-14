2226187a9d970f96025e0191ba6fd506fb5dc22c
Stefan Oltmann <github@stefan-oltmann.de>
2023-12-14 09:26:00 +0100

    Latin1EncodingTest extended

--- src/commonTest/kotlin/com/ashampoo/kim/common/Latin1EncodingTest.kt
+++ src/commonTest/kotlin/com/ashampoo/kim/common/Latin1EncodingTest.kt
@@ -16,34 +16,64 @@
 package com.ashampoo.kim.common
 
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class Latin1EncodingTest {
 
+    private val shortTestString = "Äußerst öffentliches Ü!"
+
+    private val shortTestStringLatin1Bytes =
+        byteArrayOf(
+            0xC4.toByte(), 0x75, 0xDF.toByte(), 0x65, 0x72,
+            0x73, 0x74, 0x20, 0xF6.toByte(), 0x66, 0x66,
+            0x65, 0x6E, 0x74, 0x6C, 0x69, 0x63, 0x68, 0x65,
+            0x73, 0x20, 0xDC.toByte(), 0x21
+        )
+
+    private val shortTestStringUtf8Bytes =
+        byteArrayOf(
+            0xC3.toByte(), 0x84.toByte(), 0x75, 0xC3.toByte(),
+            0x9F.toByte(), 0x65, 0x72, 0x73, 0x74, 0x20,
+            0xC3.toByte(), 0xB6.toByte(), 0x66, 0x66, 0x65,
+            0x6E, 0x74, 0x6C, 0x69, 0x63, 0x68, 0x65, 0x73,
+            0x20, 0xC3.toByte(), 0x9C.toByte(), 0x21
+        )
+
+    @Test
+    fun testEncodeToLatin1Bytes() {
+
+        assertContentEquals(
+            expected = shortTestStringLatin1Bytes,
+            actual = shortTestString.encodeToLatin1Bytes()
+        )
+    }
+
+    @Test
+    fun testEncodeToUtf8Bytes() {
+
+        assertContentEquals(
+            expected = shortTestStringUtf8Bytes,
+            actual = shortTestString.encodeToByteArray()
+        )
+    }
+
     @Test
     fun testDecodeLatin1BytesToString() {
 
-        /* ISO 8859-1 bytes */
         assertEquals(
-            "Äußerst öffentliches Ü!",
-            byteArrayOf(
-                0xC4.toByte(), 0x75, 0xDF.toByte(), 0x65, 0x72,
-                0x73, 0x74, 0x20, 0xF6.toByte(), 0x66, 0x66,
-                0x65, 0x6E, 0x74, 0x6C, 0x69, 0x63, 0x68, 0x65,
-                0x73, 0x20, 0xDC.toByte(), 0x21
-            ).decodeLatin1BytesToString()
+            expected = shortTestString,
+            actual = shortTestStringLatin1Bytes.decodeLatin1BytesToString()
         )
+    }
 
-        /* Just for comparison the UTF-8 bytes. */
+    /* Just for comparison the UTF-8 bytes. */
+    @Test
+    fun testDecodeUtf8BytesToString() {
+
         assertEquals(
-            "Äußerst öffentliches Ü!",
-            byteArrayOf(
-                0xC3.toByte(), 0x84.toByte(), 0x75, 0xC3.toByte(),
-                0x9F.toByte(), 0x65, 0x72, 0x73, 0x74, 0x20,
-                0xC3.toByte(), 0xB6.toByte(), 0x66, 0x66, 0x65,
-                0x6E, 0x74, 0x6C, 0x69, 0x63, 0x68, 0x65, 0x73,
-                0x20, 0xC3.toByte(), 0x9C.toByte(), 0x21
-            ).decodeToString()
+            expected = shortTestString,
+            actual = shortTestStringUtf8Bytes.decodeToString()
         )
     }
 }
