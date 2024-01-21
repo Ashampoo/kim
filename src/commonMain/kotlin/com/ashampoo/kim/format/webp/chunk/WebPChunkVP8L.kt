250c0b5f6d91c1bafc225d8fff79ce905b9c482a
Stefan Oltmann <github@stefan-oltmann.de>
2024-01-21 19:20:20 +0100

    Implemented WebP image size logic for VP8L

--- src/commonMain/kotlin/com/ashampoo/kim/format/webp/chunk/WebPChunkVP8L.kt
+++ src/commonMain/kotlin/com/ashampoo/kim/format/webp/chunk/WebPChunkVP8L.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright 2024 Ashampoo GmbH & Co. KG
+ * Copyright 2007-2023 The Apache Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +16,40 @@
  */
 package com.ashampoo.kim.format.webp.chunk
 
+import com.ashampoo.kim.common.ImageReadException
 import com.ashampoo.kim.format.webp.WebPChunkType
 
+@Suppress("MagicNumber")
 class WebPChunkVP8L(
     bytes: ByteArray
-) : WebPChunk(WebPChunkType.VP8L, bytes)
+) : WebPChunk(WebPChunkType.VP8L, bytes) {
+
+    val imageWidth: Int
+    val imageHeight: Int
+    val hasAlpha: Boolean
+
+    val versionNumber: Int
+
+    init {
+
+        val b1: Int = bytes[1].toInt() and 0xFF
+        val b2: Int = bytes[2].toInt() and 0xFF
+        val b3: Int = bytes[3].toInt() and 0xFF
+        val b4: Int = bytes[4].toInt() and 0xFF
+
+        imageWidth = b1 + (b2 and 63 shl 8) + 1
+        imageHeight = (b4 and 0x0F shl 10 or (b3 shl 2) or (b2 and 0xC0 shr 6)) + 1
+
+        hasAlpha = b4 and 16 != 0
+
+        versionNumber = b4 shr 5
+
+        if (versionNumber != 0)
+            throw ImageReadException("VP8L version should be 0")
+    }
+
+    override fun toString(): String =
+        super.toString() +
+            " imageWidth=$imageWidth imageHeight=$imageHeight" +
+            " hasAlpha=$hasAlpha versionNumber=$versionNumber"
+}
