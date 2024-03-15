e03a0287e8ba14249c59f62081c42b21ece080d7
Stefan Oltmann <github@stefan-oltmann.de>
2024-03-15 14:43:19 +0100

    Updated to local snapshot of XMP Core 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,13 @@ plugins {
 repositories {
     google()
     mavenCentral()
+    mavenLocal()
 }
 
 val productName = "Ashampoo Kim"
 
 val ktorVersion: String = "2.3.8"
-val xmpCoreVersion: String = "1.1.0"
+val xmpCoreVersion: String = "507c1d3"
 val dateTimeVersion: String = "0.5.0"
 val testRessourcesVersion: String = "0.4.0"
 val ioCoreVersion: String = "0.3.1"
