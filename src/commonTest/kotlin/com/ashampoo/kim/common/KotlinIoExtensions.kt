f04157ad79ebd53d55099dcd6a86040efe60ba2d
Stefan Oltmann <github@stefan-oltmann.de>
2024-04-06 10:18:25 +0200

    Fixed dependency

---
+++ src/commonTest/kotlin/com/ashampoo/kim/common/KotlinIoExtensions.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2024 Ashampoo GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ashampoo.kim.common
+
+import kotlinx.io.buffered
+import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
+import kotlinx.io.readByteArray
+
+/*
+ * Copied from ktorMain sourceset, which is not available for pure JS!
+ */
+
+@OptIn(ExperimentalStdlibApi::class)
+internal fun Path.copyTo(destination: Path) {
+
+    require(exists()) { "$this does not exist." }
+
+    val metadata = SystemFileSystem.metadataOrNull(this)
+
+    requireNotNull(metadata) { "Failed to read metadata of $this" }
+    require(metadata.isRegularFile) { "Source $this must be a regular file." }
+
+    SystemFileSystem.source(this).buffered().use { rawSource ->
+        SystemFileSystem.sink(destination).buffered().use { sink ->
+            sink.write(rawSource, metadata.size)
+        }
+    }
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+internal fun Path.writeBytes(byteArray: ByteArray) =
+    SystemFileSystem
+        .sink(this)
+        .buffered()
+        .use { it.write(byteArray) }
+
+@OptIn(ExperimentalStdlibApi::class)
+internal fun Path.readBytes(): ByteArray =
+    SystemFileSystem
+        .source(this)
+        .buffered()
+        .use { it.readByteArray() }
+
+@OptIn(ExperimentalStdlibApi::class)
+internal fun Path.exists(): Boolean =
+    SystemFileSystem.exists(this)
